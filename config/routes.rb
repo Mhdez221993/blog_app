91e998f3ddffbbbcefb34607985922d6c2600e7f
Moises <mhdezcoronado@gmail.com>
2022-01-18 16:26:05 +0100

    Optimize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
+  # get '/users/:user_id/posts', to: 'posts#index'
+  # get '/users/:user_id/posts/:id', to: 'posts#show'
 
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: [:index, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
