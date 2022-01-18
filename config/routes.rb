93f9cf1f153f7a57c8cd5afb6175274a5f79a80b
Moises <mhdezcoronado@gmail.com>
2022-01-18 12:26:24 +0100

    Create routes, actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:user_id/posts/:id', to: 'posts#show'
+
+  resources :users, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
 end
