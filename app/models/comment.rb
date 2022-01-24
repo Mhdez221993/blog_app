ffbb04169a7f234e1e7069c44c1e466cfc67d198
Moises <mhdezcoronado@gmail.com>
2022-01-24 20:02:15 +0100

    Add class and foreingn key on Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
 
-  def comments_counter_for_post(id)
-    Comment.find(id).posts.comments_counter += 1
+  def comments_counter_for_post(post)
+    post.increment!(:comments_counter)
   end
 end
