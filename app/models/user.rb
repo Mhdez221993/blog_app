5197c6af59d6bd574667064e0ebc447da385cc1c
Moises <mhdezcoronado@gmail.com>
2022-01-24 20:03:17 +0100

    Remove dependency destroy from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :posts
+  has_many :comments
+  has_many :likes
 
   def three_recent_user
     User.limit(3).order(created_at: :asc)
