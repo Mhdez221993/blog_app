6da1a984a2fa64da18decdfe57d82c4560575c19
Moises <mhdezcoronado@gmail.com>
2022-01-24 12:48:02 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_223744) do
+ActiveRecord::Schema.define(version: 2022_01_24_105754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2022_01_20_223744) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "comments_counter"
     t.integer "likes_counter"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 2022_01_20_223744) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
