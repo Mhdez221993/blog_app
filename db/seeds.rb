33729429a7c51e7746a11994b65fbe71b82b4966
Moises <mhdezcoronado@gmail.com>
2022-01-25 12:10:24 +0100

    Create user page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,10 @@
 #
 (1..10).each do |i|
   User.create([{ name: "User name #{i}", photo: "https://cdn.learnenough.com/rails.svg", bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum, odio ac interdum pharetra, velit elit dictum dui, sit amet fringilla dolor leo id justo. Suspendisse dapibus imperdiet eros non sodales. Nunc est odio, rhoncus et sodales in, mollis in lacus." }])
+
+  user = User.last
+  (1..5).each do |i|
+    user.posts.create(title: "Post ##{i}", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum, odio ac interdum pharetra...")
+  end
 end
 #   Character.create(name: "Luke", movie: movies.first)
