51772b5d98e92cd42451b2d045a30fe6c51a50a0
Moises <mhdezcoronado@gmail.com>
2022-01-26 00:27:44 +0100

    Create dinamic post show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,7 @@ class PostsController < ApplicationController
     @post = Post.find_by(user_id: params[:user_id])
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
