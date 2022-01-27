16d71ab7e906c7215eef197e9bda5ad935078dca
Moises <mhdezcoronado@gmail.com>
2022-01-27 01:45:50 +0100

    Add forms

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  before_action :current_user
+
+  def create
+    @post = Post.find(params[:post_id])
+    @user = @post.user
+    @comment = @post.comments.new(user_id: @user.id, text: commnet_params[:text])
+    if @comment.save
+      flash[:notice] = 'Comment added'
+    else
+      flash[:alert] = 'Failed to add a comment'
+    end
+    redirect_to user_post_path(@user.id, @post.id)
+  end
+
+  # private
+
+  def commnet_params
+    params.require(:comment).permit(:text)
+  end
+end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
 
-  def likes_counter_for_post(post)
+  def likes_counter
     post.increment!(:likes_counter)
   end
 end
