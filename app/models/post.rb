02ee179a489779f933b1b921dc8d10210002b3ce
Moises <mhdezcoronado@gmail.com>
2022-01-27 20:10:08 +0100

    Add validation on post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   has_many :comments, foreign_key: 'post_id'
 
+  validates :title, presence: true
+  validates :text, length: { maximum: 250, minimum: 2 }, presence: true
+  validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }, presence: true
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }, presence: true
+
   def post_counter_for_user(user)
     user.increment!(:posts_counter)
   end
