d9d9317f1a358cd7d81ddddb099861a3251f7fd7
Moises <mhdezcoronado@gmail.com>
2022-01-27 20:10:26 +0100

    Add validation to post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :name, presence: true
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, presence: true
+
   def three_recent_user
     User.limit(3).order(created_at: :asc)
   end
