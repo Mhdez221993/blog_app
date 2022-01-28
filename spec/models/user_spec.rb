fbe4479ab187076ac00bf084f4f0a176e4a1cd0e
Moises <mhdezcoronado@gmail.com>
2022-01-28 18:25:04 +0100

    Optimize test on User model using subject

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,38 +2,29 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:params) { { name: 'John', posts_counter: 0 } }
-  before(:each) do
-    User.create(params)
-    User.create(params)
-    User.create(params)
-  end
+  subject { User.new(params) }
 
   it 'is valid with valid attributes' do
-    user = User.new(name: 'John', bio: 'Jons Bio', posts_counter: 0)
-    expect(user).to be_valid
+    expect(subject).to be_valid
   end
 
   it 'is valid presence of posts_counter' do
-    user = User.new(name: 'John', bio: 'Jons Bio')
-    expect(user).to be_invalid
+    subject.posts_counter = nil
+    expect(subject).to be_invalid
   end
 
   it 'is valid posts_counter >= 0' do
-    user = User.new(name: 'John', bio: 'Jons Bio', posts_counter: -2)
-    expect(user).to be_invalid
+    subject.posts_counter = -2
+    expect(subject).to be_invalid
   end
 
   it 'is valid the presence of name' do
-    user = User.new(bio: 'Jons Bio', posts_counter: 2)
-    expect(user).to be_invalid
+    subject.name = nil
+    expect(subject).to be_invalid
   end
 
   it 'is valid name is not black' do
-    user = User.new(name: '', bio: 'Jons Bio', posts_counter: 2)
-    expect(user).to be_invalid
-  end
-
-  it 'Shoud return all active records' do
-    expect(User.count).to eq(3)
+    subject.name = ''
+    expect(subject).to be_invalid
   end
 end
