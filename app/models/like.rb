9a21bb8ba289d7f170c948696625aa419d16dffe
Moises <mhdezcoronado@gmail.com>
2022-01-28 12:40:06 +0100

    Update likes_counter after saving

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
 
+  after_save :likes_counter
+
   def likes_counter
     post.increment!(:likes_counter)
   end
