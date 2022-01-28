26eb138021e436252804a984e3800402b381d276
Moises <mhdezcoronado@gmail.com>
2022-01-28 09:54:13 +0100

    Validate posts_counter in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, presence: true
-  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def three_recent_user
     User.limit(3).order(created_at: :asc)
