14c14de1e7cc5e292ec466759c0e4e1d13fe3382
Moises <mhdezcoronado@gmail.com>
2022-01-28 18:49:46 +0100

    Test association on User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:params) { { name: 'John', posts_counter: 0 } }
-  subject { User.new(params) }
+  describe 'input data' do
+    let(:params) { { name: 'John', posts_counter: 0 } }
+    subject { User.new(params) }
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is valid presence of posts_counter' do
+      subject.posts_counter = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is valid posts_counter >= 0' do
+      subject.posts_counter = -2
+      expect(subject).to be_invalid
+    end
+
+    it 'is valid the presence of name' do
+      subject.name = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is valid name is not black' do
+      subject.name = ''
+      expect(subject).to be_invalid
+    end
   end
 
-  it 'is valid presence of posts_counter' do
-    subject.posts_counter = nil
-    expect(subject).to be_invalid
-  end
-
-  it 'is valid posts_counter >= 0' do
-    subject.posts_counter = -2
-    expect(subject).to be_invalid
-  end
-
-  it 'is valid the presence of name' do
-    subject.name = nil
-    expect(subject).to be_invalid
-  end
-
-  it 'is valid name is not black' do
-    subject.name = ''
-    expect(subject).to be_invalid
+  describe 'Association' do
+    it { should have_many(:posts) }
   end
 end
