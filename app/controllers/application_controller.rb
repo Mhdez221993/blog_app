d97d9eb4209577b744b9d4e7b7cac64a0a190613
Moises <mhdezcoronado@gmail.com>
2022-02-07 19:34:12 +0100

    Add confirmation token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
-  include Pagy::Backend
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  include Pagy::Backend
 
   protected
 
