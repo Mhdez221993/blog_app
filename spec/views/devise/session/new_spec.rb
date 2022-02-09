abd03889fe96a2eb80b1b8c04f9aa0d4699445fa
Moises <mhdezcoronado@gmail.com>
2022-02-09 15:46:47 +0100

    Test login

--- spec/views/devise/session/new_spec.rb
+++ spec/views/devise/session/new_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Signing', type: :feature do
   describe 'signin in' do
-    it 'Show the right content' do
+    before(:all) do
+      @user = User.new(name: 'Hdez', email: 'hdez@gmail.com', password: '123456')
       visit user_session_path
-      sleep(5)
-      expect(page).to have_content('Log in')
+    end
+
+    it 'I can see the username and password inputs and the "Submit" button' do
+      expect(page).to have_field(type: 'email')
+      expect(page).to have_field(type: 'password')
+      expect(page).to have_button(type: 'submit')
+    end
+
+    it 'When I click the submit button without filling in the username and the password, I get a detailed error' do
+      click 'Log in'
+      # sleep(5)
+      # expect(page).to have_content('Invalid Email or password.')
     end
   end
 end
