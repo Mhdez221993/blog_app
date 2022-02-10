e6860ce490097461f3d30cc6852a34044a8cdc99
Moises <mhdezcoronado@gmail.com>
2022-02-10 11:21:34 +0100

    verify the presence of the inputs and button

--- spec/views/devise/session/new_spec.rb
+++ spec/views/devise/session/new_spec.rb
@@ -1,22 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'Signing', type: :feature do
+RSpec.describe 'Login page', type: :feature do
   describe 'signin in' do
-    before(:all) do
-      @user = User.new(name: 'Hdez', email: 'hdez@gmail.com', password: '123456')
+    before(:each) do
       visit user_session_path
+      User.create! name: 'chaw', password: '123456', email: 'hdez@gmail.com', confirmed_at: Time.now
     end
 
     it 'I can see the username and password inputs and the "Submit" button' do
       expect(page).to have_field(type: 'email')
       expect(page).to have_field(type: 'password')
       expect(page).to have_button(type: 'submit')
-    end
-
-    it 'When I click the submit button without filling in the username and the password, I get a detailed error' do
-      click 'Log in'
-      # sleep(5)
-      # expect(page).to have_content('Invalid Email or password.')
     end
   end
 end
