7acaf301ac51f673ddffaa4fbe14d490e8080cee
Moises <mhdezcoronado@gmail.com>
2022-02-10 17:14:42 +0100

    Test Post title

---
+++ spec/views/posts/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Pos show page", type: :feature do
+  before(:each) do
+    @user = User.create! name: 'Doe', email: 'doe@gmail.com', password: '1233456', confirmed_at: Time.now
+    @post = @user.posts.create! title: 'Loren1', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    @post.comments.create! text: 'Consectetur adipiscing elit', user_id: @user.id
+    Like.create! post_id: @post.id, user_id: @user.id
+
+    visit user_session_path
+    fill_in 'Email', with: 'doe@gmail.com'
+    fill_in 'Password', with: '1233456'
+    click_on 'Log in'
+    visit user_post_path(@user, @post)
+  end
+
+  it "I can see the post's title." do
+    expect(page).to have_content('Loren1')
+  end
+end
