833d5f92e808e887763e0ef863637e2ddd5a9b43
Moises <mhdezcoronado@gmail.com>
2022-02-10 14:10:46 +0100

    Update recent post method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
-    posts.limit(3).order('created_at DESC')
+    posts.limit(3)
   end
 
   def admin?
