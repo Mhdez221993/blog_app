60be2d8d041af800e12ea138d6ee471b07289a94
Moises <mhdezcoronado@gmail.com>
2022-02-10 12:52:15 +0100

    I can see the number of posts the user has written.

--- spec/views/users/index_spec.rb
+++ spec/views/users/index_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'User index page', type: :feature do
   before(:each) do
-    @subjec1 = User.create! name: 'Hdez', email: 'hdez@gmail.com', password: '1234567', confirmed_at: Time.now
-    @subjec1 = User.create! name: 'Doe', email: 'doe@gmail.com', password: '1234567', confirmed_at: Time.now
+    User.create! name: 'Hdez', email: 'hdez@gmail.com', password: '1234567', confirmed_at: Time.now
+    User.create! name: 'Doe', email: 'doe@gmail.com', password: '1234567', confirmed_at: Time.now
+    subjec = User.first
+    subjec.posts.create! title: 'Lord', text: 'The lord of the ring'
+    subjec.posts.create! title: 'Lord', text: 'The lord of the ring'
+
     visit user_session_path
     fill_in 'Email', with: 'hdez@gmail.com'
     fill_in 'Password', with: '1234567'
@@ -17,5 +21,10 @@ RSpec.describe 'User index page', type: :feature do
   it "I can see the user's username." do
     expect(page).to have_content('Hdez')
     expect(page).to have_content('Doe')
+  end
+
+  it 'I can see the number of posts the user has written.' do
+    expect(page).to have_content('0 Posts')
+    expect(page).to have_content('2 Post')
   end
 end
