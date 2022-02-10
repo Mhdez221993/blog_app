054456df8a1c93689724693b9b492bd239f80674
Moises <mhdezcoronado@gmail.com>
2022-02-10 11:22:59 +0100

    If incorrect credential display detailed message

--- spec/views/devise/session/new_spec.rb
+++ spec/views/devise/session/new_spec.rb
@@ -12,5 +12,13 @@ RSpec.describe 'Login page', type: :feature do
       expect(page).to have_field(type: 'password')
       expect(page).to have_button(type: 'submit')
     end
+
+    it 'When I click the submit button without filling in the username and the password, I get a detailed error' do
+      visit user_session_path
+      fill_in 'Email', with: 'hdez@gmail.com'
+      fill_in 'Password', with: '1'
+      click_on 'Log in'
+      expect(page).to have_content('Invalid Email or password.')
+    end
   end
 end
