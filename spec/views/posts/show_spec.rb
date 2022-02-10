d299b811adb9a7c8d4e5eca0cf306b7494a061df
Moises <mhdezcoronado@gmail.com>
2022-02-10 17:19:22 +0100

    Test the commentors name

--- spec/views/posts/show_spec.rb
+++ spec/views/posts/show_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe "Pos show page", type: :feature do
   before(:each) do
     @user = User.create! name: 'Doe', email: 'doe@gmail.com', password: '1233456', confirmed_at: Time.now
+    @user2 = User.create! name: 'JJ', email: 'JJ@gmail.com', password: '1233456', confirmed_at: Time.now
     @post = @user.posts.create! title: 'Loren1', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
     @post.comments.create! text: 'Consectetur adipiscing elit', user_id: @user.id
+    @post.comments.create! text: 'adipiscing elit', user_id: @user2.id
     Like.create! post_id: @post.id, user_id: @user.id
 
     visit user_session_path
@@ -23,7 +25,7 @@ RSpec.describe "Pos show page", type: :feature do
   end
 
   it "I can see how many comments it has." do
-    expect(page).to have_content('1 Comment')
+    expect(page).to have_content('2 Comment')
   end
 
   it "I can see how many likes it has." do
@@ -32,5 +34,10 @@ RSpec.describe "Pos show page", type: :feature do
 
   it "I can see the post body." do
     expect(page).to have_content('Lorem ipsum dolor sit amet, consectetur adipiscing elit')
+  end
+
+  it "I can see the username of each commentor." do
+    expect(page).to have_content('Doe')
+    expect(page).to have_content('JJ')
   end
 end
