318df8876f1b973fcb737a4361e42a67ec84137f
Moises <mhdezcoronado@gmail.com>
2022-02-11 15:26:59 +0100

    Fix 'Your application has sessions disabled'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include RackSessionFixController
   include ActionController::RequestForgeryProtection
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
   before_action :authenticate_user!
