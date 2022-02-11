ab6ba15af867df0ee3a857ed2817dd508f1d9d2e
Moises <mhdezcoronado@gmail.com>
2022-02-11 16:29:11 +0100

    Create end-point to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def index
     @users = User.all.order('created_at DESC')
+    render json: @users
   end
 
   def show
     @user = User.find_by(id: params[:id])
     @posts = @user.recent_posts
+    render json: @user
   end
 end
