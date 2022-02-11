96aef5d746f5d9a5aa32d65b796ebfa6e8478d85
Moises <mhdezcoronado@gmail.com>
2022-02-11 15:07:28 +0100

    Update user model with jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable,
+         :jwt_authenticatable,
+         :registerable,
+         jwt_revocation_strategy: JwtDenylist
   has_many :posts
   has_many :comments
   has_many :likes
