f827d07c69207837a30fc313cd662a61cedb70f9
Moises <mhdezcoronado@gmail.com>
2022-02-11 17:26:45 +0100

    Fix end point to display all posts

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
   get 'likes/create'
 
-  resources :posts, only: %i[new create] do
+  resources :posts, only: %i[new create index] do
     resources :comments, only: %i[new create destroy index]
     resources :likes, only: %i[new create]
   end
