63b0325e3b21c0cffcfb090d1ea2a40d113e21c9
Moises <mhdezcoronado@gmail.com>
2022-02-15 22:49:06 +0100

    Install and setup devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pagy::Backend
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
