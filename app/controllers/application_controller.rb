bac56181303da678413eaaead9a11cbdd7101ba3
Moises <mhdezcoronado@gmail.com>
2022-02-15 22:54:01 +0100

    Can register a new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio])
   end
 end
